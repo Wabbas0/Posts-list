910d072444b386241e63ab4c442735536b71b57c
Walid <wibrahim@viktor.ai>
2023-11-19 18:35:22 +0100

    Created PostList & SortablePostItem Components and rendered mockdata

---
+++ src/components/PostList.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="post-list-container">
+    <h2 class="post-list-title">Sortable Post List</h2>
+
+    <TransitionGroup tag="ul" name="post-list" v-if="posts.length">
+      <SortablePostItem v-for="post in posts" :post="post" :key="post.id" />
+    </TransitionGroup>
+
+    <div class="post-list-empty" v-else>
+      <p>No posts found</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import SortablePostItem from './SortablePostItem.vue'
+import { ref } from 'vue'
+
+const posts = ref([
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' },
+  { id: 3, title: 'Post 3' },
+  { id: 4, title: 'Post 4' },
+  { id: 5, title: 'Post 5' }
+])
+</script>
+
+<style scoped lang="scss">
+.post-list-container {
+  @apply flex flex-col gap-6;
+}
+
+.post-list-title {
+  @apply text-white font-semibold text-3xl;
+}
+
+.post-list {
+  @apply flex;
+  flex-direction: column;
+  gap: 28px;
+  list-style: none;
+  padding-inline-start: 0;
+}
+
+.post-list-empty {
+  @apply p-4 bg-white flex justify-between;
+}
+</style>
---
+++ src/components/SortablePostItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <li class="post-item">
+    <p class="post-item-title">Post {{ props.post.id }}</p>
+    <span class="post-item-icons" :class="{ 'self-stretch': !position }">
+      <button
+        v-if="!(position === 'FIRST')"
+        :aria-label="`Move post ${props.post.id} up`"
+        :title="`Move post ${props.post.id} up`"
+        class="post-item-button"
+        @click.prevent="movePost(props.post.id, 'UP')"
+      >
+        <i class="fa-solid fa-chevron-up fa-sm"></i>
+      </button>
+      <button
+        v-if="!(position === 'LAST')"
+        :aria-label="`Move post ${props.post.id} down`"
+        :title="`Move post ${props.post.id} down`"
+        class="post-item-button"
+        @click.prevent="movePost(props.post.id, 'DOWN')"
+      >
+        <i class="fa-solid fa-chevron-down fa-sm"></i>
+      </button>
+    </span>
+  </li>
+</template>
+
+<script setup lang="ts">
+interface SortablePostItemProps {
+  post: {
+    id: number
+  }
+  position?: 'FIRST' | 'LAST'
+}
+
+const props = defineProps<SortablePostItemProps>()
+
+const movePost = (postId: number, direction: 'UP' | 'DOWN') => {
+  // Mocking the movePost function for now
+  console.log(`Mocked movePost: Move post ${postId} ${direction}`)
+}
+</script>
+
+<style scoped lang="scss">
+.post-item {
+  @apply p-3 bg-white flex items-center justify-between min-h-[6.2rem];
+  border: 1px solid #e9e9e9;
+  border-radius: var(--default-border-radius);
+  box-shadow: var(--default-box-shadow);
+  overflow: hidden;
+}
+
+.post-item-button {
+  // @apply -my-2 p-2 leading-none text-lavender-500;
+  // @apply hover:bg-lavender-200;
+  @apply my-2 p-2 leading-none;
+
+  transition: all 0.1s ease;
+  border-radius: var(--default-border-radius);
+}
+
+.post-item-icons {
+  @apply flex flex-col justify-between;
+}
+
+.post-item-title {
+  @apply text-xl;
+}
+</style>
